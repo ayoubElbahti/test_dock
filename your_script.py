df7572b0d1ab2e1b90ba463496cc2bd4a1659b33
EL BAHTI Ayoub <79053668+ayoubElbahti@users.noreply.github.com>
2024-05-04 23:25:37 +0100

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -1,13 +1,4 @@
+from instatest import Download
 
-from selenium import webdriver
-
-option = webdriver.FirefoxOptions() 
-option.add_argument("--headless")
-option.add_argument("--no-sandbox")
-driver = webdriver.Firefox(options=option)
-driver.delete_all_cookies()
-driver.implicitly_wait(13)
-print('go ')
-driver.get("https://snapinsta.app/")
-driver.quit()
-print('fin')
+dd = Download("hhh")
+print(dd.instagram())
